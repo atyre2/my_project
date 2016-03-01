c8432451ec2ef61b3cbf556100b425c1a8bf1033
Drew Tyre <atyre2@unl.edu>
2016-03-01 07:53:21 -0600

    More week 8 demos and gap-wide

---
+++ code/demo_dplyr.R
@@ -0,0 +1,66 @@
+# demonstrate dplyr
+gapminder <- read.csv("data/gapminder-FiveYearData.csv")
+
+gapminder[,"pop"] * gapminder[,"gdpPercap"]
+gapminder[ , c("pop", "gdpPercap")]
+select(gapminder, pop, gdpPercap)
+
+filter(gapminder, country == "Australia")
+
+filter(gapminder, continent == "Europe", 
+       year < 1980)
+
+Auspop <- gapminder %>%
+  filter(country == "Australia") %>%
+  select(pop,gdpPercap) %>%
+  mutate(GDP = pop * gdpPercap,
+         loggdpPercap = log10(gdpPercap))
+
+grouped.df <- iris %>% 
+  group_by(Species) %>% 
+  summarise(mean_Petal.Length = mean(Petal.Length),
+            sd_Petal.Length = sd(Petal.Length),
+            n_Species = n(),
+            se_mean = sd_Petal.Length / sqrt(n_Species))
+
+long.df <- gapminder %>% 
+  group_by(continent, year) %>% 
+  summarize(n_countries = n())
+
+library(tidyr)
+spread(long.df,continent,n_countries)
+
+iris %>% 
+  group_by(Species) %>% 
+  gather(flower_att, value = measurement, -Species)
+  
+  spread(Species,Petal.Length)
+  
+  
+# make gap_wide
+# first make gap_long!
+gap_long <- gapminder %>% 
+  gather(obstype,obs_value,-continent,-country,-year) %>% 
+  unite(obstype_year,obstype,year,sep="_") %>% 
+  select(continent,country,obstype_year,obs_value) %>% 
+  arrange(continent,country,obstype_year)
+
+# now spread into wide format
+gap_wide <- spread(gap_long,obstype_year,obs_value)
+write.csv(gap_wide,file="data/gap-wide.csv")  
+## advanced challenge:
+## continent names in reverse order
+gapminder %>%
+  count(continent) %>%
+  select(continent) %>%
+  arrange(desc(continent))
+
+## sample 2 countries from each continent
+## ?sample_n is the hint
+gapminder %>%
+  filter(year==2002) %>% 
+  group_by(continent) %>%
+  sample_n(size=2) %>% 
+  summarize(meanlifeExp = mean(lifeExp)) %>% 
+  arrange(desc(continent))
+  
